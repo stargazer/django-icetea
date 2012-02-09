8af5caacc92a507267d03fee3f2257b0d95fddc0
stargazer <already.late@gmail.com>
2012-02-09 16:25:59 +0100

    1) Documented 2) Underscore for Class variables

--- icetea/resource.py
+++ icetea/resource.py
@@ -30,36 +30,50 @@ class Resource:
     the :meth:`Resource.__call__`.
     """
     # Mappings of: {<Api Handler instance>, <model>}
-    TYPEMAPPER = {}
+    _TYPEMAPPER = {}
     DEFAULT_EMITTER_FORMAT = 'json'
 
     def __init__(self, handler):
         """
-        Initializes the Resource instance. It should appen once for every
+        Initializes the Resource instance. It should happen once for every
         Handler, in the url mapper.
 
         When requests come in, Resource.__call__ takes off.
+        
+        Data assigned on the resource instance, should be read-only. Why?
+        Concurrent requests coming on the same API endpoint, will be server by
+        the same resource. So, the same __call__ method will be called by
+        different threads of the wsgi process. If one of the threads modifies
+        the resource's attributes, the modifications will affect all threads.
+        
+        Then, is it a problem that both threads will be running the same
+        handler?        
+        No. The handlers take the ``request`` object as input, and their
+        behaviour is based on that. So, the 2 threads running the same handler,
+        will be running it with different input, and therefore different
+        behaviour.
+
 
         # Every ``Resource`` instance pairs up with a ``Handler`` instance.
         # This handler, resource pair will be responsible to handle all
         # incoming request of a certain API endpoint.
         """
         # Instantiate Handler instance
-        self.handler = handler()
+        self._handler = handler()
 
-        if getattr(self.handler, 'model', None):
-            self.TYPEMAPPER[self.handler] = self.handler.model
+        if getattr(self._handler, 'model', None):
+            self._TYPEMAPPER[self._handler] = self._handler.model
 
-        # TODO: Is this needed? Where and how is it used?
-        self.csrf_exempt = getattr(self.handler, 'csrf_exempt', True)
+        # Exempt this view from CSRF token checks
+        self.csrf_exempt = getattr(self._handler, 'csrf_exempt', True)
 
-        if not self.handler.authentication:
-            self.authentication = NoAuthentication()
+        if not self._handler.authentication:
+            self._authentication = NoAuthentication()
         else:
-            self.authentication = self.handler.authentication
+            self._authentication = self._handler.authentication
 
-        self.email_errors = getattr(settings, 'ICETEA_ERRORS', True)
-        self.display_errors = getattr(settings, 'ICETEA_DISPLAY_ERRORS', True)
+        self._email_errors = getattr(settings, 'ICETEA_ERRORS', True)
+        self._display_errors = getattr(settings, 'ICETEA_DISPLAY_ERRORS', True)
         
 
 
@@ -71,8 +85,12 @@ class Resource:
 
         It analyzes the request, executes it, packs and serializes the response, and
         sends it back to the caller.
+
+        .. note::
+            This method is invoked by the URL mapper. It is run by a separate
+            thread for every request.
         """                     
-        # Reset query list
+        # Reset (request specific) query list
         connection.queries = []
 
         # Is user authenticated?
@@ -81,8 +99,8 @@ class Resource:
 
         # Is this HTTP method allowed?
         request_method = request.method.upper()
-        if not request_method in self.handler.allowed_methods:
-            return HttpResponseNotAllowed(self.handler.allowed_methods)
+        if not request_method in self._handler.allowed_methods:
+            return HttpResponseNotAllowed(self._handler.allowed_methods)
 
         try:
             # Cleanup the request and make sure its request body is valid.
@@ -90,16 +108,17 @@ class Resource:
         except MimerDataException:
             return HttpResponseBadRequest("Invalid request body")
         
+
         # Execute request
         try:          
             # Dictionary containing {'data': <Data results in text>}                
-            response_dictionary = self.handler.execute_request(request, *args, **kwargs)
+            response_dictionary = self._handler.execute_request(request, *args, **kwargs)
 
             # Add debug messages to response dictionary
             self.response_add_debug(response_dictionary)
 
             # Serialize the result into JSON
-            serialized_result, content_type = self.serialize_result(response_dictionary, request, (), *args, **kwargs) 
+            serialized_result, content_type = self.serialize_result(response_dictionary, request, *args, **kwargs) 
 
             # Construct HTTP response       
             response = HttpResponse(serialized_result, mimetype=content_type, status=200)
@@ -113,7 +132,7 @@ class Resource:
             if isinstance(result, HttpResponse) and not result._is_string:
                 # Only for HttpResponseBadRequest, we return data.
                 serialized_result, content_type = self.serialize_result(
-                    result, request, None, *args, **kwargs)
+                    result, request, *args, **kwargs)
                 return HttpResponse(serialized_result, mimetype=content_type, \
                     status=result.status_code)
             elif isinstance(result, HttpResponse):
@@ -124,13 +143,11 @@ class Resource:
         #   response['Content-Disposition'] = 'attachment; filename=Smart.pr-export-%s.xls' % date
             
 
-    def serialize_result(self, result, request, fields, *args, **kwargs):
+    def serialize_result(self, result, request, *args, **kwargs):
         """
         @param result: Result of the execution of the handler, wrapped in a
-        dictionary with keys 'data' and (maybe) 'debug'.
+        dictionary where keys and values are simply text.
         @param request: Request object
-        @param fields: If handler is a ModelHandler, fields is a list of fields
-        to output. Else ().
         """
         # What's the emitter format to use?
         emitter_format = self.determine_emitter_format(request, *args, **kwargs)
@@ -140,14 +157,12 @@ class Resource:
         emitter_class, content_type = Emitter.get(emitter_format)
 
         # create instance of the emitter class
-        serializer = emitter_class(self.TYPEMAPPER, result, self.handler, fields)
+        serializer = emitter_class(self._TYPEMAPPER, result, self._handler, None)
         # serialize the result
         serialized_result = serializer.render(request)
 
         return serialized_result, content_type
  
- 
-
     def determine_emitter_format(self, request, *args, **kwargs):
         """
         Returns the emitter format.
@@ -172,7 +187,7 @@ class Resource:
         authentication).
         False otherwise.
         """
-        if self.authentication.is_authenticated(request):
+        if self._authentication.is_authenticated(request):
             return True
         else:
             return False 
@@ -202,19 +217,6 @@ class Resource:
                 else:
                     request.data = request.PUT
 
-
-    def email_exception(self, reporter):
-        subject = "Piston crash report"
-        html = reporter.get_traceback_html()
-        message = EmailMessage(
-            settings.EMAIL_SUBJECT_PREFIX + subject,
-            html,
-            settings.SERVER_EMAIL,
-            [admin[1] for admin in settings.ADMINS]
-        )
-        message.content_subtype = 'html'
-        # Modify it if it works!
-        message.send(fail_silently=False)
 
     def error_handler(self, e, request):
         """
@@ -256,14 +258,27 @@ class Resource:
                 exc_value,
                 traceback.tb_next
             )
-            if self.email_errors:
+            if self._email_errors:
                 self.email_exception(reporter)
-            if self.display_errors:
+            if self._display_errors:
                 return HttpResponseServerError(
                     format_error('\n'.join(reporter.format_exception())))
             else:
                 raise
             # TODO: Give 500 error. Crash report.
+
+
+    def email_exception(self, reporter):
+        subject = "Piston crash report"
+        html = reporter.get_traceback_html()
+        message = EmailMessage(
+            settings.EMAIL_SUBJECT_PREFIX + subject,
+            html,
+            settings.SERVER_EMAIL,
+            [admin[1] for admin in settings.ADMINS]
+        )
+        message.content_subtype = 'html'
+        message.send(fail_silently=False)
 
         
     def response_add_debug(self, response_dictionary):
