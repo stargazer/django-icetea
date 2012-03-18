ab140b844424298d5a357722c6944e009ad8a867
stargazer <already.late@gmail.com>
2012-03-18 02:30:45 +0100

    Added excel emitter

--- icetea/emitters.py
+++ icetea/emitters.py
@@ -16,6 +16,8 @@ from django.core.serializers.json import DateTimeAwareJSONEncoder
 from django.http import HttpResponse
 from django.core import serializers
 
+import xlwt, StringIO
+
 # Class which will register MimeTypes to methods which will decode the
 # corresponding MimeType to python data structures.
 from utils import Mimer
@@ -95,11 +97,11 @@ class Emitter:
             """
             Dispatch, all types are routed through here.
 
-			@param thing: Data we are trying to serialize
-			@param fields: The fields of ``thing`` to serialize. Applies only
+            @param thing: Data we are trying to serialize
+            @param fields: The fields of ``thing`` to serialize. Applies only
             for database models.
-			@param nested: Are the fields of ``thing`` nested, or are they
-			first class fields?
+            @param nested: Are the fields of ``thing`` nested, or are they
+            first class fields?
             """        
             ret = None
 
@@ -131,7 +133,7 @@ class Emitter:
             """
             Foreign keys.
 
-			Fields of foreign keys are always nested.
+            Fields of foreign keys are always nested.
             """
             return _any(getattr(data, field.name), fields=(), nested=True)
 
@@ -247,7 +249,7 @@ class Emitter:
         def _qs(data, fields=(), nested=False):
             """
             Querysets.
-			``data`` is a queryset
+            ``data`` is a queryset
 
             Queryset data might as well be nested or first class fields.
             """
@@ -266,10 +268,11 @@ class Emitter:
             IF the values of the dictionary are models or querysets, they
             should appear as nested.
             """
-            # If there is no field selection, just output everything.
             if fields:
+                # If there is no field selection, just output everything.
                 return dict([ (k, _any(v, fields, nested)) for k, v in data.iteritems() if k in fields])
-            return dict([ (k, _any(v, fields, nested)) for k, v in data.iteritems() ])
+            else:
+                return dict([ (k, _any(v, fields, nested)) for k, v in data.iteritems()])
         
 
         # If the handler is a BaseHandler, any models should appear as nested
@@ -360,3 +363,95 @@ class DjangoEmitter(Emitter):
         return response
 
 Emitter.register('django', DjangoEmitter, 'text/xml; charset=utf-8')
+
+class ExcelEmitter(Emitter):
+    def render(self, request):
+        def _to_unicode(string):
+            """ Return the unicode repsesentation of string"""
+            try:
+                return unicode(string)
+            except UnicodeDecodeError:
+                # the string is a bytestring
+                ascii_text = str(string).encode('string_escape')
+                return unicode(ascii_text)
+        def to_utf8(string):
+            """Return the utf-8 encoded representation of the string """
+            unic = _to_unicode(string)
+            return unic.encode('utf-8')
+
+        # In the case of the ExcelEmitter, we want only the actual data. No
+        # debug messages and shit
+        data = self.data['data']
+        # Fields that we should output. In theory, these should be the only
+        # keys in the dictionary ``data``.
+        fields = self.handler.get_output_fields(request)
+
+        wb = xlwt.Workbook(encoding='utf-8')
+        stream = StringIO.StringIO()
+        
+        ws = wb.add_sheet("SmartPR")
+        
+        # Write field names on row 0
+        col = 0
+        for field_name in fields:
+            ws.write(0, col, field_name.capitalize())
+            col = col + 1
+
+        # In case the ``data`` is a dictionary (eg the request was asking for a
+        # single model instance), we transform it to a list
+        if isinstance(data, dict):
+            data = [data]
+
+        row = 1     
+
+        for record in data:
+            # every record is a dict
+            
+            col = 0         
+            for key in fields:
+                value = ""
+                field_value = record[key]
+                
+                # I merge lists or dicts to "\r\n"-separated strings
+                # Why? 
+                # 1. They look better (i think)
+                # 2. They non-ASCII chars appear correctly
+                if isinstance(field_value, list):
+                    if field_value:
+                        field_value = [to_utf8(item) for item in field_value]
+                        value = "\r\n".join(field_value)
+                elif isinstance(field_value, dict):
+                    if field_value:
+                        value = "\r\n".join(to_utf8(key) + ":" + to_utf8(value)  for key, value in
+                            field_value.items())
+                else:
+                    value = to_utf8(record[key])
+
+                ws.write(row, col, value )
+                col = col + 1
+            row = row + 1
+        wb.save(stream)
+        return stream.getvalue()
+    # TODO
+    # Works only for outputting handlers extending the ModelHandler class
+    # Doesn't really work with outputing nested fields 
+
+Emitter.register('excel', ExcelEmitter, 'application/vnd.ms-excel')
+ 
+               
+class HTMLEmitter(Emitter):
+    def render(self, request):
+        construct = self.construct()
+
+        if 'data' in construct:
+            # Correct response
+            return construct['data']
+
+        elif 'errors' in construct:
+            # Validation was raised
+            return construct['errors']
+
+        return None         
+        
+
+Emitter.register('html', HTMLEmitter, 'text/html')
