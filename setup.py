874e20492770ffca84e192d3a68555e4c1a4fc28
stargazer <already.late@gmail.com>
2012-05-06 11:46:24 +0200

    Version 0.3.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 setup(
 	name="django-icetea",
 	packages=('icetea', ),
-	version="0.3",
+	version="0.3.2",
     description="REST API Framework",
 	author="C. Paschalides",
 	author_email="already.late@gmail.com",
